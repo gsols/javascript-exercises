d6859c2c814d2a6b6243629a4068464f765316b4
Glenn Solis <glennpescaderosolis@gmail.com>
2026-01-28 21:50:54 +0800

    start 6-9

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, a) {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === Args){
+            arr.splice(i, 1);
+            i--; // Adjust index after removal
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    if (a < 0 || b < 0) return 'ERROR';
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+
+
+    if (a > b){
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    console.log(a, b);
+    for(let i = a; i<=b; i++){
+        sum += i;
+    }
+    return sum; 
 };
 
 // Do not edit below this line
