8c2c7aea061ae365f75890801b6e6fe5b0753555
Glenn Solis <glennpescaderosolis@gmail.com>
2026-02-10 12:45:51 +0800

    complete 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) return "OOPS";
+    if (typeof a === "string") a = +a;
+    if (a === 0) return 0;
+    if (a === 1) return 1;
+    return fibonacci(+a - 1) + fibonacci(+a - 2);
 };
 
 // Do not edit below this line
