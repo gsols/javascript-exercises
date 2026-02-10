fc699320bc4bad3895a424778ba21ee60f47000c
Glenn Solis <glennpescaderosolis@gmail.com>
2026-02-10 12:37:41 +0800

    complete 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanedString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
