e4bf840eacc1ce63fb69065e4c2c25b22c402d95
Glenn Solis <glennpescaderosolis@gmail.com>
2026-02-10 12:59:32 +0800

    complete 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const age = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+        return age > oldest.age ? { name: person.name, age } : oldest;
+    }, { name: "", age: 0 });
 };
 
 // Do not edit below this line
